2b6f92b2782c93044259a11ad34e7b48a74268f0
PhilipAtEmcogma <emcogma@gmail.com>
2024-03-29 12:32:31 +1300

    use Cargo fmt, to formated code

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use crate::my_functions::add_five;
 use crate::other_funcs::minus_funcs::subtract_ten;
 //use crate::my_functions::* //importing every function in the library
 
-
 //use crate::my_functions::{add_five, subtract_ten};
 //functions can be imported as a bulk like above, or individually like below
 /*
@@ -15,7 +14,7 @@ use create::my_functions::add_five;
 use create::my_functions::subtrate_10;
 */
 
-fn main(){
+fn main() {
     //println!("Hello, World!");
     let x: u32 = 50;
     println!("x is {}", x);
--- src/my_functions.rs
+++ src/my_functions.rs
@@ -1,21 +1,20 @@
 //making the function as publich using the keyword 'pub', will allow the function to be called else where
-pub fn add_five(num: u32) -> u32{
-    num+5
+pub fn add_five(num: u32) -> u32 {
+    num + 5
 }
 
 //the line below indicates to rust that it will be used as unit testing for this module (file)
 #[cfg(test)]
-mod test{
+mod test {
     use super::*;
 
     #[test]
-    fn add_five_test(){
+    fn add_five_test() {
         let x = 100;
         let y = add_five(x);
         println!("y is from test: {}", y);
 
         // return true or false to indicate did the function passed the unit test
         assert_eq!(y, 105);
-
     }
 }
