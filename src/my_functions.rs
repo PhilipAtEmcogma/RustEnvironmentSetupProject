50e7cab2931ca76633ecff41744bb391c1bd9b5d
PhilipAtEmcogma <emcogma@gmail.com>
2024-03-29 11:52:38 +1300

    basic unit testing example added

--- src/my_functions.rs
+++ src/my_functions.rs
@@ -3,3 +3,19 @@ pub fn add_five(num: u32) -> u32{
     num+5
 }
 
+//the line below indicates to rust that it will be used as unit testing for this module (file)
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    #[test]
+    fn add_five_test(){
+        let x = 100;
+        let y = add_five(x);
+        println!("y is from test: {}", y);
+
+        // return true or false to indicate did the function passed the unit test
+        assert_eq!(y, 105);
+
+    }
+}
--- src/other_funcs/minus_funcs.rs
+++ src/other_funcs/minus_funcs.rs
@@ -1,3 +1,20 @@
 pub fn subtract_ten(num: u32) -> u32{
-    num+10
+    num-10
+}
+
+//unit testing part
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    #[test]
+    fn subtract_ten_test(){
+        let x = 100;
+        let y = subtract_ten(x);
+        println!("y is from test: {}", y);
+
+        // return true or false to indicate did the function passed the unit test
+        assert_eq!(y, 90);
+        
+    }
 }
