dc2697ff9d2c9ca0b28ec4aee4b9bab3e33f324f
PhilipAtEmcogma <emcogma@gmail.com>
2024-03-29 00:25:25 +1300

    first basic function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+
+fn add_five(num: u32) -> u32{
+    num+5
+}
+
+fn main(){
+    //println!("Hello, World!");
+    let x: u32 = 50;
+    println!("x is {}", x);
+    let y = add_five(x);
+    println!("y is {}", y);
 }
