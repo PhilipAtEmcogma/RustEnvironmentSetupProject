8dc8b81b8924c8b847bcf1b4c85157ab2499937e
PhilipAtEmcogma <emcogma@gmail.com>
2024-03-29 13:11:45 +1300

    added example of Documentation generation

--- src/other_funcs/minus_funcs.rs
+++ src/other_funcs/minus_funcs.rs
@@ -1,5 +1,20 @@
+/* use /// to create documentation (like below), 
+    then use 'cargo doc' to build the documentation, which cargo will generate the documentations and save if into doc folder
+    finally use 'cargo doc --open' to open the documentation for this rust project.
+    very useful for documentating the project.
+*/
+///Function: subtract_ten
+/// 
+/// # Arguments (num:u32)
+///
+/// # Example
+/// ```
+/// let x = 15;
+/// let y= subtract_ten(x);
+/// ```
+
 pub fn subtract_ten(num: u32) -> u32 {
-    num - 10
+    num - 10 //no need ; for the return part
 }
 
 //unit testing part
